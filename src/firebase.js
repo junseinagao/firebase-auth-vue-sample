6c4c57ba5020a5f8ed6c35a822c403ae430dcf45
junseinagao <junsei.nagao@gmail.com>
2021-06-11 13:37:59 +0900

    firebaseを追加

---
+++ src/firebase.js
@@ -0,0 +1,13 @@
+import firebase from "firebase"
+import "firebase/firestore"
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDcSXTfTQ6rsYu6NS467aCDctg2b7niP0w",
+  authDomain: "webexpert-demo-project.firebaseapp.com",
+  projectId: "webexpert-demo-project",
+  storageBucket: "webexpert-demo-project.appspot.com",
+  messagingSenderId: "321948403209",
+  appId: "1:321948403209:web:2e2689356ff070564e1ad8",
+}
+
+firebase.initializeApp(firebaseConfig)
